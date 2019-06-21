2056ab784a027c583c3fe06233052873b27afae3
Anggun Pribadi <anggun.pribadi23@gmail.com>
2019-06-21 10:50:23 +0700

    order eres

--- application/controllers/Home.php
+++ application/controllers/Home.php
@@ -1,13 +1,15 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 class Home extends CI_Controller
 {
-    function __construct() {
+    function __construct()
+    {
         parent::__construct();
     }
 
-    function index() {
+    function index()
+    {
         $treatment_popular = $this->app_model->get_data_query("SELECT * FROM product WHERE product_most_popular = 1 ORDER BY  product_most_popular_date DESC LIMIT 4")->result();
         $treatment_promo = $this->app_model->get_data_query("SELECT * FROM product WHERE product_promo_list = 1 ORDER BY product_promo_list_date DESC LIMIT 2")->result();
         $treatment_new = $this->app_model->get_data_query("SELECT * FROM product WHERE product_new_in = 1 ORDER BY product_new_in_date DESC LIMIT 2")->result();
@@ -24,9 +26,10 @@ class Home extends CI_Controller
         $this->load->view('frontend/layout/app', $data);
     }
 
-    function detail() {
+    function detail()
+    {
         $url = $this->uri->segment(2);
-        $detail_product = $this->app_model->get_data_query("SELECT a.*, b.product_category_name, b.product_category_url  FROM product a LEFT JOIN product_category b ON b.product_category_id = a.product_category_id WHERE a.product_url = '".$url."' ")->row();
+        $detail_product = $this->app_model->get_data_query("SELECT a.*, b.product_category_name, b.product_category_url  FROM product a LEFT JOIN product_category b ON b.product_category_id = a.product_category_id WHERE a.product_url = '" . $url . "' ")->row();
         $menu = $this->app_model->get_data_query("SELECT * FROM product_category WHERE product_category_parent = 0 AND product_category_status = 'active'")->result();
         $submenu = $this->app_model->get_data_query("SELECT product_id, product_category_id, product_name, product_url FROM product WHERE product_status = 'active'")->result();
         $data = array(
@@ -38,10 +41,11 @@ class Home extends CI_Controller
         $this->load->view('frontend/layout/app', $data);
     }
 
-    function pages() {
+    function pages()
+    {
         $url = $this->uri->segment(2);
-        $row_id = $this->app_model->get_data_query("SELECT * FROM product_category WHERE product_category_url = '".$url."' ")->row();
-        $list_product = $this->app_model->get_data_query("SELECT * FROM product WHERE product_category_id = '".$row_id->product_category_id."' ")->result();
+        $row_id = $this->app_model->get_data_query("SELECT * FROM product_category WHERE product_category_url = '" . $url . "' ")->row();
+        $list_product = $this->app_model->get_data_query("SELECT * FROM product WHERE product_category_id = '" . $row_id->product_category_id . "' ")->result();
         $menu = $this->app_model->get_data_query("SELECT * FROM product_category WHERE product_category_parent = 0 AND product_category_status = 'active'")->result();
         $submenu = $this->app_model->get_data_query("SELECT product_id, product_category_id, product_name, product_url FROM product WHERE product_status = 'active'")->result();
 
@@ -51,8 +55,7 @@ class Home extends CI_Controller
                 'menu'    => $menu,
                 'submenu' => $submenu
             );
-        } 
-        else if ($url == 'outlet') {
+        } else if ($url == 'outlet') {
             $this->load->library('googlemaps');
             $config = array();
             $config['center'] = "-6.2449033, 106.9658942";
@@ -78,15 +81,16 @@ class Home extends CI_Controller
                 'product' => $list_product
             );
         }
-        
+
         $this->load->view('frontend/layout/app', $data);
     }
 
-    function order(){
+    function order()
+    {
         $url = $this->uri->segment(2);
         $menu = $this->app_model->get_data_query("SELECT * FROM product_category WHERE product_category_parent = 0 AND product_category_status = 'active'")->result();
         $submenu = $this->app_model->get_data_query("SELECT product_id, product_category_id, product_name, product_url FROM product WHERE product_status = 'active'")->result();
-        $detail_product = $this->app_model->get_data_query("SELECT a.*, b.product_category_name, b.product_category_url  FROM product a LEFT JOIN product_category b ON b.product_category_id = a.product_category_id WHERE a.product_url = '".$url."' ")->row();
+        $detail_product = $this->app_model->get_data_query("SELECT a.*, b.product_category_name, b.product_category_url  FROM product a LEFT JOIN product_category b ON b.product_category_id = a.product_category_id WHERE a.product_url = '" . $url . "' ")->row();
         $payment = $this->app_model->get_data_query("SELECT * FROM payment_type WHERE payment_type_status = 'active' ")->result();
         $data = array(
             'content' => 'frontend/order',
@@ -98,5 +102,4 @@ class Home extends CI_Controller
 
         $this->load->view('frontend/layout/app', $data);
     }
-    
 }
