043c43534915d6dc4833b158046864362072d356
Anggun Pribadi <anggun.pribadi23@gmail.com>
2019-07-18 08:39:07 +0700

    new

--- application/views/frontend/outlet.php
+++ application/views/frontend/outlet.php
@@ -2,7 +2,6 @@
     <div class="header-page">
         <div class="container">
             <h1>OUR OUTLET</h1>
-            <p>Jalan Assyafiiyah RT 03/01 No. 134 Kelurahan Jakasampurna Kecamatan Bekasi Barat, Kota Bekasi 17145</p>
         </div>
     </div>
     <!-- /.header-page -->
